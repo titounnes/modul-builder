e92e41f1c9a678bfde5d1012d764e62460a0eb0a
titounnes <zano.amrhakim@gmail.com>
2015-04-05 16:27:45 +0700

    Create pagination.php

---
+++ application/config/pagination.php
@@ -0,0 +1,18 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+$config['per_page'] = 20;
+$config['num_links'] = 0;
+
+$config['use_page_numbers'] = FALSE;
+$config['display_pages'] = FALSE;
+$config['next_link'] = 'Next';
+$config['prev_link'] = 'Prev';
+$config['first_tag_open'] = '<li>';
+$config['first_tag_close'] = '</li>';
+$config['next_tag_open'] = '<li>';
+$config['next_tag_close'] = '</li>';
+$config['prev_tag_open'] = '<li>';
+$config['prev_tag_close'] = '</li>';
+$config['last_tag_open'] = '<li>';
+$config['last_tag_close'] = '</li>';
+$config['use_global_url_suffix'] = TRUE;
