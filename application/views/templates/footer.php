2b33558b27a70c83458aded233e07941efd69d0c
titounnes <zano.amrhakim@gmail.com>
2015-04-05 16:28:36 +0700

    Create footer.php

---
+++ application/views/templates/footer.php
@@ -0,0 +1,7 @@
+                <em>&copy; 2014</em>
+        <script src="<?php echo base_url('assets/jquery/jquery.min.js')?>"></script>
+		<script src="<?php echo base_url('assets/jquery/jquery.dataTables.min.js')?>"></script>
+		<script src="<?php echo base_url('assets/jquery/dataTables.jqueryui.js')?>"></script>
+		<script src="<?php echo base_url('assets/bootstrap/js/bootstrap.min.js')?>"></script>
+		</body>
+</html>
